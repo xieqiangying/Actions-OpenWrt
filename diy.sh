7a657641677076a499b766f3167ce17296315eb2
未日 <33367701+RealKiro@users.noreply.github.com>
2020-02-09 16:46:45 +0800

    add more plugin

--- diy.sh
+++ diy.sh
@@ -6,43 +6,43 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为Argon
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-git https://github.com/sypopo/luci-theme-argon-mc.git package/mine/luci-theme-argon_new
-sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
+# git https://github.com/sypopo/luci-theme-argon-mc.git package/mine/luci-theme-argon_new
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 
 # 复杂的AdGuardHome的openwrt的luci界面
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
 
 # KPR plus+
-git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
+# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
 
 # Server酱
-git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
 
 # 硬盘监控
-git clone https://github.com/project-openwrt/luci-app-smartinfo.git package/mine/luci-app-smartinfo
-mv package/mine/luci-app-smartinfo/luci-dir/applications/luci-app-smartinfo package/mine/luci-app-smartinfo/
+# git clone https://github.com/project-openwrt/luci-app-smartinfo.git package/mine/luci-app-smartinfo
+# mv package/mine/luci-app-smartinfo/luci-dir/applications/luci-app-smartinfo package/mine/luci-app-smartinfo/
 
 # FileBrowser
-git clone https://github.com/project-openwrt/FileBrowser.git package/mine/FileBrowser
+# git clone https://github.com/project-openwrt/FileBrowser.git package/mine/FileBrowser
 
 # 网易云音乐
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/mine/luci-app-unblockneteasemusic
 
 # 网易云音乐GoLang版本
-git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-go.git package/mine/luci-app-unblockneteasemusic-go
+# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-go.git package/mine/luci-app-unblockneteasemusic-go
 
 # 网易云音乐mini
-# git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/mine/luci-app-unblockneteasemusic-mini
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini.git package/mine/luci-app-unblockneteasemusic-mini
 
 # disable usb3.0
 # git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/mine/luci-app-usb3disable
 
 # 管控上网行为
-git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/mine/OpenAppFilter
 
 # Rclone-OpenWrt
-git clone https://github.com/ElonH/Rclone-OpenWrt.git package/mine/Rclone-OpenWrt
+# git clone https://github.com/ElonH/Rclone-OpenWrt.git package/mine/Rclone-OpenWrt
