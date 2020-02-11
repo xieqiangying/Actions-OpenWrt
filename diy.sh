2f0543a8ed6d4ef720751b5494d722e4b0800a70
未日 <33367701+RealKiro@users.noreply.github.com>
2020-02-11 20:00:48 +0800

    diskman : replace smartinfo

--- diy.sh
+++ diy.sh
@@ -10,21 +10,21 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 
 # 更改默认主题为Argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-git https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
+git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/mine/luci-theme-argon_new
 sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/Makefile
 
 # 复杂的AdGuardHome的openwrt的luci界面
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
+
+# DiskMan for LuCI (WIP)
+git clone https://github.com/lisaac/luci-app-diskman.git package/mine/luci-app-diskman
+mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
 
 # KPR plus+
 git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/mine/luci-app-koolproxyR
 
 # Server酱
 git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
-
-# 硬盘监控
-git clone https://github.com/project-openwrt/luci-app-smartinfo.git package/mine/luci-app-smartinfo
-mv package/mine/luci-app-smartinfo/luci-dir/applications/luci-app-smartinfo package/mine/luci-app-smartinfo/
 
 # FileBrowser
 git clone https://github.com/project-openwrt/FileBrowser.git package/mine/FileBrowser
